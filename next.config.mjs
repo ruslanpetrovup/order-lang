d39c5b4b5f7ddc55af5bce98db7b69d07364a859
ruslanpetrovup <ruslanpetrov163@gmail.com>
2025-01-25 17:03:00 +0200

    test 4

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig = {
   images: {
     domains: ['localhost'],
-    output: 'standalone',
     remotePatterns: [
       {
         protocol: 'http',
